bdf887389e71659f80b254ce379d1068a0527ef2
Ralph Slooten <axllent@gmail.com>
2023-05-05 16:49:59 +1200

    Bugfix: Fix Date display when message doesn't contain a Date header

--- storage/database.go
+++ storage/database.go
@@ -491,7 +491,7 @@ func GetMessage(id string) (*Message, error) {
 
 			logger.Log().Debugf("[db] %s does not contain a date header, using received datetime", id)
 
-			date = time.UnixMicro(created)
+			date = time.UnixMilli(created)
 		}); err != nil {
 			logger.Log().Error(err)
 		}
