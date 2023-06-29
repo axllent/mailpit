c1b6e02d48ee9d913b298f6c4a36b80376f5ccf0
Júnior Messias <jrmessias@gmail.com>
2023-06-29 14:30:47 -0300

    :splarkles: Theme toggler
    Add toggler to change theme (light, dark, auto)

--- server/ui-src/app.js
+++ server/ui-src/app.js
@@ -3,5 +3,6 @@ import App from './App.vue';
 import "./assets/styles.scss";
 import "../../node_modules/bootstrap-icons/font/bootstrap-icons.scss";
 import "bootstrap";
+import "./color-modes";
 
 createApp(App).mount('#app');
