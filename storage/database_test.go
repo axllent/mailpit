a63952aee649213748ed91d972875912f6600c07
Ralph Slooten <axllent@gmail.com>
2023-09-24 17:29:27 +1300

    Tests: Add search delete tests

--- storage/database_test.go
+++ storage/database_test.go
@@ -1,16 +1,13 @@
 package storage
 
 import (
-	"bytes"
 	"fmt"
-	"io/ioutil"
-	"math/rand"
+	"os"
 	"testing"
 	"time"
 
 	"github.com/axllent/mailpit/config"
 	"github.com/axllent/mailpit/utils/logger"
-	"github.com/jhillyerd/enmime"
 )
 
 var (
@@ -110,11 +107,11 @@ func TestRetrieveMimeEmail(t *testing.T) {
 	}
 
 	assertEqual(t, msg.From.Name, "Sender Smith", "\"From\" name does not match")
-	assertEqual(t, msg.From.Address, "sender@example.com", "\"From\" address does not match")
+	assertEqual(t, msg.From.Address, "sender2@example.com", "\"From\" address does not match")
 	assertEqual(t, msg.Subject, "inline + attachment", "subject does not match")
 	assertEqual(t, len(msg.To), 1, "incorrect number of recipients")
 	assertEqual(t, msg.To[0].Name, "Recipient Ross", "\"To\" name does not match")
-	assertEqual(t, msg.To[0].Address, "recipient@example.com", "\"To\" address does not match")
+	assertEqual(t, msg.To[0].Address, "recipient2@example.com", "\"To\" address does not match")
 	assertEqual(t, len(msg.Attachments), 1, "incorrect number of attachments")
 	assertEqual(t, msg.Attachments[0].FileName, "Sample PDF.pdf", "attachment filename does not match")
 	assertEqual(t, len(msg.Inline), 1, "incorrect number of inline attachments")
@@ -133,76 +130,6 @@ func TestRetrieveMimeEmail(t *testing.T) {
 		t.Fail()
 	}
 	assertEqual(t, len(inlineData.Content), msg.Inline[0].Size, "inline attachment size does not match")
-}
-
-func TestSearch(t *testing.T) {
-	setup()
-	defer Close()
-
-	t.Log("Testing search")
-	for i := 0; i < testRuns; i++ {
-		msg := enmime.Builder().
-			From(fmt.Sprintf("From %d", i), fmt.Sprintf("from-%d@example.com", i)).
-			Subject(fmt.Sprintf("Subject line %d end", i)).
-			Text([]byte(fmt.Sprintf("This is the email body %d <jdsauk;dwqmdqw;>.", i))).
-			To(fmt.Sprintf("To %d", i), fmt.Sprintf("to-%d@example.com", i))
-
-		env, err := msg.Build()
-		if err != nil {
-			t.Log("error ", err)
-			t.Fail()
-		}
-
-		buf := new(bytes.Buffer)
-
-		if err := env.Encode(buf); err != nil {
-			t.Log("error ", err)
-			t.Fail()
-		}
-
-		if _, err := Store(buf.Bytes()); err != nil {
-			t.Log("error ", err)
-			t.Fail()
-		}
-	}
-
-	for i := 1; i < 51; i++ {
-		// search a random something that will return a single result
-		searchIdx := rand.Intn(4) + 1
-		var search string
-		switch searchIdx {
-		case 1:
-			search = fmt.Sprintf("from-%d@example.com", i)
-		case 2:
-			search = fmt.Sprintf("to-%d@example.com", i)
-		case 3:
-			search = fmt.Sprintf("\"Subject line %d end\"", i)
-		default:
-			search = fmt.Sprintf("\"the email body %d jdsauk dwqmdqw\"", i)
-		}
-
-		summaries, _, err := Search(search, 0, 100)
-		if err != nil {
-			t.Log("error ", err)
-			t.Fail()
-		}
-
-		assertEqual(t, len(summaries), 1, "1 search result expected")
-
-		assertEqual(t, summaries[0].From.Name, fmt.Sprintf("From %d", i), "\"From\" name does not match")
-		assertEqual(t, summaries[0].From.Address, fmt.Sprintf("from-%d@example.com", i), "\"From\" address does not match")
-		assertEqual(t, summaries[0].To[0].Name, fmt.Sprintf("To %d", i), "\"To\" name does not match")
-		assertEqual(t, summaries[0].To[0].Address, fmt.Sprintf("to-%d@example.com", i), "\"To\" address does not match")
-		assertEqual(t, summaries[0].Subject, fmt.Sprintf("Subject line %d end", i), "\"Subject\" does not match")
-	}
-
-	// search something that will return 200 results
-	summaries, _, err := Search("This is the email body", 0, testRuns)
-	if err != nil {
-		t.Log("error ", err)
-		t.Fail()
-	}
-	assertEqual(t, len(summaries), testRuns, "search results expected")
 }
 
 func BenchmarkImportText(b *testing.B) {
@@ -241,12 +168,12 @@ func setup() {
 
 	var err error
 
-	testTextEmail, err = ioutil.ReadFile("testdata/plain-text.eml")
+	testTextEmail, err = os.ReadFile("testdata/plain-text.eml")
 	if err != nil {
 		panic(err)
 	}
 
-	testMimeEmail, err = ioutil.ReadFile("testdata/mime-attachment.eml")
+	testMimeEmail, err = os.ReadFile("testdata/mime-attachment.eml")
 	if err != nil {
 		panic(err)
 	}
