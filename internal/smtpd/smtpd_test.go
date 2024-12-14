9f4908d11d8701da20c5cd024f45903a67512e60
Thomas Landauer <thomas@landauer.at>
2024-12-14 19:56:20 +0100

    Add case-insensitive flags to regex'es (#411)
    * Update smtpd.go: Adding case-insensitive flags to regex'es
    * Update smtpd_test.go

--- internal/smtpd/smtpd_test.go
+++ internal/smtpd/smtpd_test.go
@@ -84,8 +84,8 @@ func TestCmdHELO(t *testing.T) {
 
 	// Verify that HELO resets the current transaction state like RSET.
 	// RFC 2821 section 4.1.4 says EHLO should cause a reset, so verify that HELO does it too.
-	cmdCode(t, conn, "MAIL FROM:<sender@example.com>", "250")
-	cmdCode(t, conn, "RCPT TO:<recipient@example.com>", "250")
+	cmdCode(t, conn, "mail from:<sender@example.com>", "250") // Also testing case-insensitivity
+	cmdCode(t, conn, "rcpt to:<recipient@example.com>", "250")
 	cmdCode(t, conn, "HELO host.example.com", "250")
 	cmdCode(t, conn, "DATA", "503")
 
