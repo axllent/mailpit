b078e14dc905a8cd3868382448dda100ffa5b506
Matt Currie <me@mattcurrie.com>
2025-04-29 11:38:17 +1200

    Display unread count in app badge

---
+++ server/ui-src/components/AppBadge.vue
@@ -0,0 +1,33 @@
+<script>
+import { mailbox } from '../stores/mailbox.js'
+
+export default {
+    computed: {
+        mailboxUnread() {
+            return mailbox.unread
+        }
+    },
+
+    watch: {
+        mailboxUnread: {
+            handler(newUnread) {
+                this.updateAppBadge(newUnread)
+            },
+            immediate: true
+        }
+    },
+
+    methods: {
+        updateAppBadge(unreadCount) {
+            if (!('setAppBadge' in navigator)) {
+                return
+            }
+
+            navigator.setAppBadge(unreadCount)
+        }
+    }
+
+}
+</script>
+
+<template></template>
